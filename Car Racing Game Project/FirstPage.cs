9cb52615696dc75b62ec87991c55585bb4963872
Sandip Kumbhar <kumbharsandip66@gmail.com>
2025-07-04 18:16:29 +0530

    CarRacingGame

---
+++ Car Racing Game Project/FirstPage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Car_Racing_Game_Project
+{
+    public partial class FirstPage : Form
+    {
+        public FirstPage()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            Car_Select C = new Car_Select();
+
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Please enter player name");
+            }
+            else
+            {
+                this.Hide();
+                C.Show();
+            }
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
